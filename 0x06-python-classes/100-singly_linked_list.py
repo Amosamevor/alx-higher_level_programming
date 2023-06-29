2421777561eebfb0a5f43623d537b9fadefbefe4
AmosAmevor <amosamevor@gmail.com>
2023-06-29 00:12:22 +0000

    7. Singly linked list correction

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -32,7 +32,37 @@ class Node:
 
     @next_node.setter
     def next_node(self, value):
-        if not isinstance(value, Node) and value tmp = tmp.next_node
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """Represent a singly-linked list."""
+
+    def __init__(self):
+        """Initalize a new SinglyLinkedList."""
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """Insert a new Node to the SinglyLinkedList.
+        The node is inserted into the list at the correct
+        ordered numerical position.
+        Args:
+            value (Node): The new Node to insert.
+        """
+        new = Node(value)
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            tmp = self.__head
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
+                tmp = tmp.next_node
             new.next_node = tmp.next_node
             tmp.next_node = new
 
@@ -43,34 +73,4 @@ class Node:
         while tmp is not None:
             values.append(str(tmp.data))
             tmp = tmp.next_node
-        return ('\n'.join(values)s not None:
-                            raise TypeError("next_node must be a Node object")
-                                    self.__next_node = value
-
-
-                                    class SinglyLinkedList:
-                                        """Represent a singly-linked list."""
-
-                                            def __init__(self):
-                                                    """Initalize a new SinglyLinkedList."""
-                                                            self.__head = None
-
-                                                                def sorted_insert(self, value):
-                                                                        """Insert a new Node to the SinglyLinkedList.
-                                                                                The node is inserted into the list at the correct
-                                                                                        ordered numerical position.
-                                                                                                Args:
-                                                                                                            value (Node): The new Node to insert.
-                                                                                                                    """
-                                                                                                                            new = Node(value)
-                                                                                                                                    if self.__head is None:
-                                                                                                                                                new.next_node = None
-                                                                                                                                                            self.__head = new
-                                                                                                                                                                    elif self.__head.data > value:
-                                                                                                                                                                                new.next_node = self.__head
-                                                                                                                                                                                            self.__head = new
-                                                                                                                                                                                                    else:
-                                                                                                                                                                                                                tmp = self.__head
-                                                                                                                                                                                                                            while (tmp.next_node is not None and
-                                                                                                                                                                                                                                                    tmp.next_node.data < value):
-                                                                                                                                                                                                                                           )
+        return ('\n'.join(values))
