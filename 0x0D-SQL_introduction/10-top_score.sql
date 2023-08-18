b3828503d382717b19c2bd9318505ae4c8bbca79
AmosAmevor <amosamevor@gmail.com>
2023-08-18 02:34:52 +0000

    0x0D-SQL_introduction

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- List by best
+-- List all records of the `second_table` by `score` in descending order
+SELECT score, name FROM `second_table` ORDER BY score desc;-- List by best
+-- List all records of the `second_table` by `score` in descending order
+SELECT score, name FROM `second_table` ORDER BY score desc;
